f6e87264bb9226d7bbd7653e33b3e6afe5bc142c
Kajcin22 <masekkaterina@gmail.com>
2022-06-01 17:43:21 +0200

    vytvořena landing-page (momentálně na odkaze statistika)

--- pages/landing-page/index.jsx
+++ pages/landing-page/index.jsx
@@ -18,21 +18,66 @@ export default function Home() {
           </div>
 
           <div className={styles.header__signIn}>
-            <button
-              onClick={() => setOpened(true)}
-              className={styles.header__btn}
-            >
-              Přihlášení
-            </button>
             <Image
               src={'/user-icon.jpg'}
               alt="prihlaseni"
               width={35}
               height={35}
             />
+            <button
+              onClick={() => setOpened(true)}
+              className={styles.header__btn}
+            >
+              Přihlášení
+            </button>
           </div>
         </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.hero_image}>
+          <div>
+            <h3>Kniha je okno do celého vesmíru</h3>
+            <p>probuď své sny každodenním čtením</p>
+            <button className={styles.header__btn}>Číst nyní!</button>
+          </div>
+
+          <Image src="/reading-book.png" width={250} height={200} />
+        </div>
+        <div className={styles.book_section}>
+          <div className={styles.book_section_heading}>
+            <h3>Populární knihy</h3>
+            <p>procházet</p>
+          </div>
+          <div className={styles.book_section_cards}>
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+          </div>
+        </div>
+        <div className={styles.book_section}>
+          <div className={styles.book_section_heading}>
+            <h3>Naposledy čteno</h3>
+            <p>procházet</p>
+          </div>
+          <div className={styles.book_section_cards}>
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+          </div>
+        </div>
+        <div className={styles.registrace}>
+          <h1>Zaregistruj se nyní a měj svou četbu pod kontrolou!</h1>
+          <form>
+            <label>
+              <input type="email" placeholder="zadej svůj email" />
+
+              <button className={styles.header__btn}>Registrovat</button>
+            </label>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
