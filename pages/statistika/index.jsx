909467a8226c5cacf97457e90d58a3ef902b9d11
Kajcin22 <masekkaterina@gmail.com>
2022-06-08 22:59:40 +0200

    vytvořeno doporučování knih a progres čtení

--- pages/statistika/index.jsx
+++ pages/statistika/index.jsx
@@ -3,9 +3,10 @@ import {
   getFinishedPages,
   getFinishedBooks,
   getBooksInProgress,
+  getRecommendation,
 } from '../../lib/api';
 import { useAuth } from '../../components/AuthProvider/auth-provider';
-
+import BookPreview from '../../components/BookPreview/book-preview';
 import { Progress } from '@mantine/core';
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
   const [finishedPages, setFinishedPages] = useState(null);
   const [finishedBooks, setFinishedBooks] = useState(null);
   const [booksInProgress, setBooksInProgress] = useState(null);
+  const [recommendation, setRecommendation] = useState(null);
 
   useEffect(() => {
     if (userId) {
@@ -34,28 +36,35 @@ export default function Home() {
       );
     }
   }, [userId]);
+
+  useEffect(() => {
+    if (userId) {
+      getRecommendation(userId).then((response) => setRecommendation(response));
+    }
+  }, [userId]);
+
   return (
     <>
       <h1>Statistika</h1>
       <h3>Celkem přečteno stránek: {finishedPages}</h3>
       <h3>Celkem přečteno knížek: {finishedBooks}</h3>
+      <h3>Progres čtení:</h3>
       {booksInProgress?.map((book) => {
         const value = ((book.bookmark.pageNum / book.pageNumber) * 100).toFixed(
           2,
         );
 
         return (
-          <>
+          <div key={book.id}>
             <p>{book.title}</p>
-            <Progress
-              key={book.id}
-              value={value}
-              label={`${value}%`}
-              size="xl"
-              radius="xl"
-            />
-          </>
+            <Progress value={value} label={`${value}%`} size="xl" radius="xl" />
+          </div>
         );
+      })}
+
+      <h3>Doporučení na další četbu:</h3>
+      {recommendation?.map((book) => {
+        return <BookPreview key={book.id} book={book} />;
       })}
     </>
   );
