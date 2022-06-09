399f78273f7100e3bd2615878805ebe9010ccfef
Kajcin22 <masekkaterina@gmail.com>
2022-06-09 23:00:45 +0200

    před přihlášením redirect z navigace na landing page

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,19 @@ import Layout from '../components/Layout/layout';
 import { AuthProvider } from '../components/AuthProvider/auth-provider';
 import { AddedBooksProvider } from '../components/AddedBooksProvider/added-books-provider';
 import 'swiper/css';
+import { useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+import { supabase } from '../lib/supabase_client';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!supabase.auth.user() && router.pathname !== '/') {
+      router?.push('/');
+    }
+  }, [router?.pathname]);
+
   return (
     <AuthProvider>
       <AddedBooksProvider>
